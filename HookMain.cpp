bbfb73a8bc158abc318972cc231e4e719d1d9170
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2023-11-04 02:42:59 +0900

    HookMain.exe

---
+++ HookMain.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#define DEF_DLL_NAME L"KeyHook.dll"
+#define DEF_HOOKSTART "HookStart"
+#define DEF_HOOKSTOP "HookStop"
+
+typedef void (*PFN_HOOKSTART)();
+typedef void (*PFN_HOOKSTOP)();
+
+int main(void) {
+    HINSTANCE hDll = NULL;
+    PFN_HOOKSTART HookStart = NULL;
+    PFN_HOOKSTOP HookStop = NULL;
+    char ch = 0;
+
+    // KeyHook.dll Load
+    hDll = LoadLibrary(DEF_DLL_NAME);
+
+    if (hDll == NULL) {
+        printf("Failed to load DLL.\n");
+        return 1;
+    }
+
+    // Export Function Address
+    HookStart = (PFN_HOOKSTART)GetProcAddress(hDll, DEF_HOOKSTART);
+    HookStop = (PFN_HOOKSTOP)GetProcAddress(hDll, DEF_HOOKSTOP);
+
+    if (HookStart == NULL || HookStop == NULL) {
+        printf("Failed to get function addresses.\n");
+        FreeLibrary(hDll);
+        return 1;
+    }
+
+    // Hooking Start
+    HookStart();
+
+    printf("Press 'q' to quit.\n");
+
+    // Wait until User input 'q'
+    while (_getch() != 'q');
+
+    // Hooking Stop
+    HookStop();
+
+    // KeyHook.dll Unloading
+    FreeLibrary(hDll);
+}
